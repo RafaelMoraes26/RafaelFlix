ed1bc91b115d6e42686c0edc3a31e6f753a727ba
Rafael Moraes <rafaelgmoraes2601@gmail.com>
2020-07-27 14:47:36 -0300

    Correção

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Menu from './components/Menu';
 import dadosIniciais from './data/dados_iniciais.json';
 import BannerMain from './components/BannerMain';
 import Carousel from './components/Carousel';
-import Footer from './components/Footer';
 
 function App() {
 
@@ -21,8 +20,6 @@ function App() {
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
       />
-
-      <Carousel category={dadosIniciais.categorias[0]}/>
 
       <Carousel category={dadosIniciais.categorias[1]}/>
 
